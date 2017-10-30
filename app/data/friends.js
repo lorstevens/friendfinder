08efb83345e9bcef2508a77d543804358693aa8b
lorstevens <lornacstevens@gmail.com>
2017-10-30 16:25:18 -0500

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,83 @@
+var friends = [
+{
+  "name":"Andy",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      4,
+      1,
+      3,
+      4,
+      6,
+      1,
+      2,
+      2,
+      3,
+      2
+    ]
+},
+
+{
+  "name":"Leslie",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      4,
+      1,
+      3,
+      4,
+      6,
+      1,
+      2,
+      2,
+      3,
+      2
+    ]
+},
+
+{
+  "name":"Tom",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      4,
+      1,
+      3,
+      4,
+      6,
+      1,
+      2,
+      2,
+      3,
+      2
+    ]
+},
+
+
+{
+  "name":"Ron",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      4,
+      1,
+      3,
+      4,
+      6,
+      1,
+      2,
+      2,
+      3,
+      2
+    ]
+},
+
+];
+
+// friends.forEach(myFunction);
+
+// function myFunction (item, index) {
+
+// for( var scores in item ) {
+//     console.log(item[scores])
+//   }
+// }
+
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,38 @@
+
+	var friends = require("../data/friends.js");
+	var bodyParser = require('body-parser');
+	var path = require('path');
+
+
+	module.exports = function(app) {
+
+	app.get("/api/friends", function(req, res) {
+		res.json(friends);
+	});
+
+	app.post("/api/friends", function(req, res) {
+		var newFriend = req.body 
+		console.log(newFriend);
+		friends.push(newFriend);
+		res.json(newFriend);
+
+
+		var newFriendScores = req.body.scores;
+		console.log(newFriendScores)
+
+		//looping through the scores in 
+	// 	friends.forEach(myFunction);
+
+	// 	function myFunction (item, index) {
+	// 	for (var scores in item) {
+ //    		console.log(item[scores])
+ //    		} 			
+	// }
+
+		// for (i > 0; i = newFriend.length; i++) {
+		// 	if (newFriend[i] === friends
+		// }
+
+	});
+
+};
